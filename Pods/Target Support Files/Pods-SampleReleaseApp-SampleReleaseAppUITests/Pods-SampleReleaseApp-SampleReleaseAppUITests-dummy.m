e0f44da7ccd203074451fbb93ae1d518be50d575
Marcos Ortiz <m.ortiz@m1finance.com>
2021-08-19 11:34:54 -0500

    Added pod packages fastlane and workflow

---
+++ Pods/Target Support Files/Pods-SampleReleaseApp-SampleReleaseAppUITests/Pods-SampleReleaseApp-SampleReleaseAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SampleReleaseApp_SampleReleaseAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SampleReleaseApp_SampleReleaseAppUITests
+@end
