7e4c03ed58912dc83ab34ee15f7ddf94f3e6c751
Dominic Lanzillotta <32912858+iAmericanBoy@users.noreply.github.com>
2022-04-08 13:43:47 -0400

    Revert "Update SampleReleaseAppTests.swift"

--- SampleReleaseAppTests/SampleReleaseAppTests.swift
+++ SampleReleaseAppTests/SampleReleaseAppTests.swift
@@ -12,7 +12,6 @@ class SampleReleaseAppTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        
     }
 
     override func tearDownWithError() throws {
